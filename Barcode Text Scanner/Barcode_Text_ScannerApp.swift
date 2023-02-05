092d2a1dfccb3304dd6cfb70a48ae49c839a0bb5
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2023-02-05 20:30:21 +0200

    Initial Commit

---
+++ Barcode Text Scanner/Barcode_Text_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_Text_ScannerApp.swift
+//  Barcode Text Scanner
+//
+//  Created by KhaleD HuSsien on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_Text_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
